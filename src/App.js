50195e7198437acbe51eed123f69b86de4c94a40
IT2 <it2@AWT.local>
2020-06-11 14:25:00 -0500

    wip, added hash scroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from "react";
-import NavBar from "./components/NavBar"
-
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
-  
-
   return (
-    <div className="h-screen">
-      <div className="lg:flex text-md mt-16 ml-16">
-        <div className="lg:mr-64">
-        <h2 className="text-gray-500">Pedro Revilla</h2>
-        <h2 className="italic text-gray-700">Full Stack Developer</h2>
-        </div>
-        <div className="ml-32 mt-16 lg:ml-64 lg:mt-0">
-          <NavBar/>
+    <div>
+      <div className="h-screen">
+        <div className="lg:flex text-md mt-16 ml-16">
+          <div className="lg:mr-64">
+            <h2 className="text-gray-700">Pedro Revilla</h2>
+            <h2 className="italic text-gray-800">Full Stack Developer</h2>
+          </div>
+          <div className="ml-32 mt-16 lg:ml-64 lg:mt-0">
+            <NavBar />
+          </div>
         </div>
       </div>
+      <section className="h-screen" id="about">
+        <About />
+      </section>
+      <section className="h-screen" id="skills">
+        <Skills />
+      </section>
+      <section className="h-screen" id="projects">
+        <Projects />
+      </section>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Scrollchor from 'react-scrollchor';
+
 
 const NavBar = () => {
     return (
         <ul>
-            <h3 className="text-gray-700">About</h3>
-            <h3 className="text-gray-700">Skills</h3>
-            <h3 className="text-gray-700">Projects</h3>
-            <h3 className="text-gray-700">Resume</h3>
+            <Scrollchor to='#about' ><h3 className="text-gray-800">About</h3></Scrollchor>
+            <Scrollchor to='#skills' ><h3 className="text-gray-800">Skills</h3></Scrollchor>
+            <Scrollchor to='#projects' ><h3 className="text-gray-800">Projects</h3></Scrollchor>
+            <Scrollchor to='#resume' ><h3 className="text-gray-800">Resume</h3></Scrollchor>
         </ul>
     )
 }
