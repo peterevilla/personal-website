373728604fceebbe550eb2fa9cb9a33fa4db7b94
IT2 <it2@AWT.local>
2020-06-11 14:48:58 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Typical from "react-typical";
 
 function App() {
   return (
     <div>
       <div className="h-screen">
-        <div className="lg:flex text-md mt-16 ml-16">
-          <div className="lg:mr-64">
-            <h2 className="text-gray-700">Pedro Revilla</h2>
-            <h2 className="italic text-gray-800">Full Stack Developer</h2>
+        <div className="lg:flex  text-md mt-16 ml-16">
+          <div className="lg:mr-64 lg:w-1/4 text-lg">
+            <h2 className=" text-gray-700">Pedro Revilla</h2>
+            {/* <h2 className="italic text-gray-800">Full Stack Developer</h2> */}
+            <Typical
+              steps={["Full Stack", 1000, "Full Stack Developer", 500]}
+              loop={Infinity}
+              wrapper="p"
+            />
           </div>
           <div className="ml-32 mt-16 lg:ml-64 lg:mt-0">
             <NavBar />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ import Scrollchor from 'react-scrollchor';
 const NavBar = () => {
     return (
         <ul>
-            <Scrollchor to='#about' ><h3 className="text-gray-800">About</h3></Scrollchor>
-            <Scrollchor to='#skills' ><h3 className="text-gray-800">Skills</h3></Scrollchor>
-            <Scrollchor to='#projects' ><h3 className="text-gray-800">Projects</h3></Scrollchor>
-            <Scrollchor to='#resume' ><h3 className="text-gray-800">Resume</h3></Scrollchor>
+            <Scrollchor to='#about' ><h3 className="text-gray-800 text-lg">About</h3></Scrollchor>
+            <Scrollchor to='#skills' ><h3 className="text-gray-800 text-lg">Skills</h3></Scrollchor>
+            <Scrollchor to='#projects' ><h3 className="text-gray-800 text-lg">Projects</h3></Scrollchor>
+            <Scrollchor to='#resume' ><h3 className="text-gray-800 text-lg">Resume</h3></Scrollchor>
         </ul>
     )
 }
