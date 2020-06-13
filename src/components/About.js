7eb190dc527555c51f52cddfcabe1a4a255b8ae0
Pedro Revilla <peterevilla@gmail.com>
2020-06-13 16:26:08 -0500

    wip

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import avatar from '../images/peepAvatar.png'
+import Fade from "react-reveal/Fade";
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className='flex flex-wrap lg:flex-no-wrap justify-center object-center'>
+            <Fade right>
+            <div className='mt-64'><h1>Some Text about me</h1></div>
+            </Fade>
+            <Fade right>
+            <img className='mt-64' src={avatar} alt="avatar"/>
+            </Fade>
         </div>
     )
 }
