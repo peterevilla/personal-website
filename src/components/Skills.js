b919ba80cd1b582c6fb5238662e62d20e5d59424
IT2 <it2@AWT.local>
2020-06-16 10:43:41 -0500

    addind cards

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import SkillsCard from './SkillsCard'
 
 const Skills = () => {
     return (
-        <div>
-            Skills
+        <div className="flex justify-center">
+        <div className="grid grid-cols-1 mt-32 lg:grid-cols-3 gap-4">
+            <SkillsCard/>
+            <SkillsCard/>
+            <SkillsCard/>
+        </div>
         </div>
     )
 }
---
+++ src/components/SkillsCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SkillsCard = () => {
+  return (
+    <div class="lg:max-w-sm rounded overflow-hidden lg:hover:shadow-lg w-full">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SkillsCard;
