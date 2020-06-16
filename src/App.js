6f439d19423a8183364ab1b6c73c810f5961aebe
Pedro Revilla <peterevilla@gmail.com>
2020-06-15 20:32:32 -0500

    font changed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Typical from "react-typical";
-import GithubIcon from './components/icons/GithubIcon.js';
-import InstagramIcon from './components/icons/InstagramIcon.js';
-import LinkedInIcon from './components/icons/LinkedInIcon.js';
+import GithubIcon from "./components/icons/GithubIcon.js";
+import InstagramIcon from "./components/icons/InstagramIcon.js";
+import LinkedInIcon from "./components/icons/LinkedInIcon.js";
 
 // const bg = require('./bg/bg.png')
 // style={{ backgroundImage: "url("+bg+")" }}
@@ -14,23 +14,41 @@ function App() {
   return (
     <div>
       <div className="h-screen">
-        <div className="lg:flex  text-md mt-16 ">
-          <div className="lg:mr-64 lg:w-1/4 ml-16  text-lg">
-            <Typical
-              steps={["Hi,", 1000, "Hi, I am Pedro Revilla", 500]}
-              wrapper="p"
-            />
+        <div className="lg:flex  mt-16 ">
+          <div className="lg:mr-64 lg:w-1/4 ml-16 ">
+            <div className="font-bold">
+              <Typical
+                steps={["Hi,", 1000, "Hi, I am Pedro Revilla", 500]}
+                wrapper="p"
+              />
+            </div>
 
-            <h2 className=" font-bold italic">Software Engineer</h2>
+            <h2 className="text-blue-700 text-bold">Software Engineer</h2>
             {/* <h2 className="italic text-gray-800">Full Stack Developer</h2> */}
           </div>
           <div>
             <NavBar />
+            <ul className="ml-32 mt-32 lg:ml-16 lg:mt-16">
+              <a href="mailto:webmaster@example.com">peterevilla@gmail.com</a>
+              <div className="flex lg:justify-center p-2">
+                <a href="https://github.com/peterevilla" className="m-2">
+                  <GithubIcon />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/pedro-revilla/"
+                  className="m-2"
+                >
+                  <LinkedInIcon />
+                </a>
+                <a href="https://www.instagram.com/peterx22/" className="m-2">
+                  <InstagramIcon />
+                </a>
+              </div>
+            </ul>
           </div>
         </div>
-       
       </div>
-     
+
       <section className="h-screen" id="about">
         <About />
       </section>
@@ -41,13 +59,19 @@ function App() {
         <Projects />
       </section>
       <footer className="flex flex-col items-center">
-              <h2>Let's grab a coffee</h2>
-              <div className="lg:flex justify-center mb-8 p-4">
-              <a href="https://github.com/peterevilla" className="m-2"><GithubIcon /></a>
-              <a href="https://www.linkedin.com/in/pedro-revilla/" className="m-2"><LinkedInIcon/></a>
-              <a href="https://www.instagram.com/peterx22/" className="m-2"><InstagramIcon/></a>
-              </div>
-        </footer>
+        <h2>Let's grab a coffee</h2>
+        <div className="flex justify-center mb-8 p-4">
+          <a href="https://github.com/peterevilla" className="m-2">
+            <GithubIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/pedro-revilla/" className="m-2">
+            <LinkedInIcon />
+          </a>
+          <a href="https://www.instagram.com/peterx22/" className="m-2">
+            <InstagramIcon />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,25 +17,25 @@ const NavBar = () => {
     <div>
       {!isScrolled ? (
         <Fade top >
-          <ul className="ml-32 mt-16 lg:ml-64 lg:mt-0">
+          <ul className="ml-32 mt-16 lg:ml-16 lg:mt-0">
             <Scrollchor to="#about">
-              <h3 className=" text-lg hover:text-green-600 transition ease-in duration-500">
+              <h3 className=" hover:text-blue-700 hover:underline transition ease-in duration-200">
                 About
               </h3>
             </Scrollchor>
 
             <Scrollchor to="#skills">
-              <h3 className=" text-lg hover:text-green-600 transition ease-in duration-500">
+              <h3 className="  hover:text-blue-700 hover:underline transition ease-in duration-500">
                 Skills
               </h3>
             </Scrollchor>
             <Scrollchor to="#projects">
-              <h3 className=" text-lg hover:text-green-600 transition ease-in duration-500">
+              <h3 className="  hover:text-blue-700 hover:underline transition ease-in duration-500">
                 Projects
               </h3>
             </Scrollchor>
             <Scrollchor to="#resume">
-              <h3 className=" text-lg hover:text-green-600 transition ease-in duration-500">
+              <h3 className="  hover:text-blue-700 hover:underline transition ease-in duration-500">
                 Resume
               </h3>
             </Scrollchor>
@@ -43,24 +43,24 @@ const NavBar = () => {
         </Fade>
       ) : (
         <Fade bottom >
-          <nav className="flex z-10  fixed ml-12   top-0 lg:flex-col lg:fixed lg:inset-y-0 lg:right-0 lg:mr-16  lg:justify-center">
+          <nav className="flex z-10 fixed ml-12   top-0 lg:flex-col lg:fixed lg:inset-y-0 lg:right-0 lg:mr-16  lg:justify-center">
             <Scrollchor to="#about">
-              <h3 className=" text-lg mx-2 hover:text-green-600 transition ease-in duration-500">
+              <h3 className="  mx-2 hover:text-blue-700 hover:underline transition ease-in duration-500">
                 About
               </h3>
             </Scrollchor>
             <Scrollchor to="#skills">
-              <h3 className=" text-lg mx-2 hover:text-green-600 transition ease-in duration-500">
+              <h3 className="  mx-2 hover:text-blue-700 hover:underline transition ease-in duration-500">
                 Skills
               </h3>
             </Scrollchor>
             <Scrollchor to="#projects">
-              <h3 className=" text-lg mx-2 hover:text-green-600 transition ease-in duration-500">
+              <h3 className="  mx-2 hover:text-blue-700 hover:underline transition ease-in duration-500">
                 Projects
               </h3>
             </Scrollchor>
             <Scrollchor to="#resume">
-              <h3 className=" text-lg mx-2 hover:text-green-600 transition ease-in duration-500">
+              <h3 className="  mx-2 hover:text-blue-700 hover:underline transition ease-in duration-500">
                 Resume
               </h3>
             </Scrollchor>
