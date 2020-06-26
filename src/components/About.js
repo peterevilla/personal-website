c94488c38e9eb0a59adb9de29d4dc3bbdce27e42
Pedro Revilla <peterevilla@gmail.com>
2020-06-26 15:34:33 -0500

    fix navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import avatar from '../images/avatar.svg'
+import React from "react";
+import avatar from "../images/avatar.svg";
 import Fade from "react-reveal/Fade";
 
 const About = () => {
-    return (
-        <div className='flex flex-wrap lg:flex-no-wrap justify-center object-center'>
-            <Fade right>
-            <div className='mt-64'><h1>Some Text about me</h1></div>
-            </Fade>
-            <Fade right>
-            <img className='mt-64' src={avatar} alt="avatar"/>
-            </Fade>
+  return (
+    <div className="flex flex-wrap lg:flex-no-wrap justify-center object-center">
+      <Fade right>
+        <div className="mt-64 w-3/6 text-justify">
+          <p>
+            Hello! I'm Pedro, a software engineer based in Chicago,IL.
+            <br /><br />I enjoy creating things that live on the internet, whether
+            that be websites, applications, or anything in between. My goal is
+            to always build products that provide pixel-perfect, performant
+            experiences.
+          </p>
         </div>
-    )
-}
+      </Fade>
+      <Fade right>
+        <img className="mt-64" src={avatar} alt="avatar" />
+      </Fade>
+    </div>
+  );
+};
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
   window.addEventListener("scroll", (event) => {
-    if (window.scrollY !== 0) {
+    if (window.scrollY > 250) {
       setIsScrolled(true);
     } else {
       setIsScrolled(false);
@@ -15,7 +15,7 @@ const NavBar = () => {
 
   return (
     <div>
-      {!isScrolled ? (
+      
         <Fade top >
           <ul className="ml-32 mt-16 lg:ml-16 lg:mt-0">
             <Scrollchor to="#about">
@@ -41,9 +41,9 @@ const NavBar = () => {
             </Scrollchor>
           </ul>
         </Fade>
-      ) : (
-        <Fade bottom >
-          <nav className="flex z-10 fixed ml-12   top-0 lg:flex-col lg:fixed lg:inset-y-0 lg:right-0 lg:mr-16  lg:justify-center">
+    {isScrolled &&
+        <Fade>
+          <nav className=" flex z-10 px-12 bg-orange-400 lg:ml-12 fixed  lg:bg-transparent top-0  lg:flex-col lg:fixed lg:inset-y-0 lg:right-0 lg:mr-16  lg:justify-center">
             <Scrollchor to="#about">
               <h3 className="  mx-2 hover:text-blue-700 hover:underline transition ease-in duration-500">
                 About
@@ -66,7 +66,7 @@ const NavBar = () => {
             </Scrollchor>
           </nav>
         </Fade>
-      )}
+}
     </div>
   );
 };
