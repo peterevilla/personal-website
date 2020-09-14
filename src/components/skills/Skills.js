b729db123804f8627e3916292c933e42618ba74b
Pedro Revilla <peterevilla@gmail.com>
2020-09-14 18:48:24 -0500

    new bg

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,10 +1,10 @@
 import React from "react";
 import SkillsCard from "./SkillsCard";
 import { SkillsData } from "./SkillsData";
-
+const bg = require("../../bg/bg2.svg");
 const Skills = () => {
   return (
-    <div>
+    <div style={{ backgroundImage: "url(" + bg + ")"}}>
       <h1 className="text-3xl text-center pt-8 ">Skills</h1>
       <div className="flex  justify-center">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 border-t pt-2">
