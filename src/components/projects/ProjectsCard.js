d8cfe9e540946f41b571643cd35f7bd9ff128dcd
Pedro Revilla <peterevilla@gmail.com>
2020-09-14 21:52:28 -0500

    added transition

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -3,7 +3,9 @@ import React from "react";
 
 const ProjectsCard = ({project}) => {
   return (
-    <div class="lg:max-w-sm rounded overflow-hidden shadow hover:shadow-lg w-full">
+    <div class="lg:max-w-sm rounded overflow-hidden shadow hover:shadow-lg w-full
+    transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-80
+    ">
       <div class="px-6 py-4">
   <div class="font-bold text-xl mb-2">{project.title}</div>
   <p class="text-gray-700 text-base">{project.info}</p>
