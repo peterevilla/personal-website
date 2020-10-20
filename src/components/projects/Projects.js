28c034f49a558dde91673fd079ebd4b32548de07
Pedro Revilla <peterevilla@gmail.com>
2020-10-19 22:02:11 -0500

    wip

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,9 +5,9 @@ import {projectsData} from "./projectsData"
 const Projects = () => {
   return (
     <div>
-      <h1 className="text-3xl text-center pt-12 mt-56 border-b w-full">Projects</h1>
+      <h1 className="text-3xl text-center pt-12 mt-56 border-b">Projects</h1>
       <div className="flex  justify-center">
-        <div className="grid grid-cols-1 lg:grid-cols-3 px-48 gap-4  pt-4 ">
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:px-48 gap-4  pt-4 ">
           {projectsData.map(items => (
             <ProjectsCard project={items}/>
           ))}
